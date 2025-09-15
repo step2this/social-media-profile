2b94be58560245e2123511407b0f7b8d1e0f4186
Steve Shapero <shaperosteve@meta.com>
2025-09-15 16:59:02 +0100

    Initial commit

---
+++ test/social-media-profile.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SocialMediaProfile from '../lib/social-media-profile-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/social-media-profile-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SocialMediaProfile.SocialMediaProfileStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
